fd7e82f3bd4fd9ac7eb2bd8eaaac3724762e79a9
Seyfeddin Bassarac <seyfeddinbassarac@teknasyon.com>
2024-03-07 21:11:45 +0300

    Add xcframework

---
+++ GTCUI.xcframework/ios-arm64_x86_64-simulator/GTCUI.framework/Headers/GTCUI.h
@@ -0,0 +1,18 @@
+//
+//  GTCUI.h
+//  GTCUI
+//
+//  Created by Hüseyin Metin on 22.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GTCUI.
+FOUNDATION_EXPORT double GTCUIVersionNumber;
+
+//! Project version string for GTCUI.
+FOUNDATION_EXPORT const unsigned char GTCUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTCUI/PublicHeader.h>
+
+
